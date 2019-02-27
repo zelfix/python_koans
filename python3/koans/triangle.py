fbff5b569184d982dcd60b635ccfbb51ed1c8d30
Protsenko Andrey <aprotsenko@ozon.ru>
2019-02-27 10:57:21 +0300

    about_triangle_project.py completed

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,7 +18,14 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    same_side_count = len(set([a, b, c]))
+    triangle_map = {
+        1: 'equilateral',
+        2: 'isosceles',
+        3: 'scalene'
+    }
+    return triangle_map[same_side_count]
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
