4000ee8d2b1753fc35001e76c2a5f8c3b3086b48
Protsenko Andrey <aprotsenko@ozon.ru>
2019-02-28 12:32:36 +0300

    about_with_statements.py completed

--- python3/koans/about_with_statements.py
+++ python3/koans/about_with_statements.py
@@ -22,7 +22,7 @@ class AboutWithStatements(Koan):
             self.fail()
 
     def test_counting_lines(self):
-        self.assertEqual(__, self.count_lines("example_file.txt"))
+        self.assertEqual(4, self.count_lines("example_file.txt"))
 
     # ------------------------------------------------------------------
 
@@ -41,7 +41,7 @@ class AboutWithStatements(Koan):
             self.fail()
 
     def test_finding_lines(self):
-        self.assertEqual(__, self.find_line("example_file.txt"))
+        self.assertEqual('test\n', self.find_line("example_file.txt"))
 
     ## ------------------------------------------------------------------
     ## THINK ABOUT IT:
@@ -85,13 +85,18 @@ class AboutWithStatements(Koan):
             return len(file.readlines())
 
     def test_counting_lines2(self):
-        self.assertEqual(__, self.count_lines2("example_file.txt"))
+        self.assertEqual(4, self.count_lines2("example_file.txt"))
 
     # ------------------------------------------------------------------
 
     def find_line2(self, file_name):
         # Using the context manager self.FileContextManager, rewrite this
         # function to return the first line containing the letter 'e'.
+        with self.FileContextManager(file_name) as file:
+            for line in file.readlines():
+                match = re.search('e', line)
+                if match:
+                    return line
         return None
 
     def test_finding_lines2(self):
@@ -105,4 +110,4 @@ class AboutWithStatements(Koan):
             return len(file.readlines())
 
     def test_open_already_has_its_own_built_in_context_manager(self):
-        self.assertEqual(__, self.count_lines3("example_file.txt"))
+        self.assertEqual(4, self.count_lines3("example_file.txt"))
